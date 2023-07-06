130cc51fa00841f97fecc079f572fc608e09b115
Anitha_97 <anithaks993@gmail.com>
2023-07-06 14:43:43 +1200

    Added Razor project

--- WebApplication3/Controllers/CategoryController.cs
+++ WebApplication3/Controllers/CategoryController.cs
@@ -19,25 +19,66 @@ namespace WebApplication3.Controllers
 
         public IActionResult Create()
         {
+
             return View();
+
         }
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Added successfully";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
             {
-                ModelState.AddModelError("displayorder", "The displayOrder cannot exactly match the name.");
+                return NotFound();
             }
-            if (obj.Name.ToLower() == "test")
+            Category? categoryObjDb = _db.Categories.Find(Id);
+            if (categoryObjDb == null)
             {
-                ModelState.AddModelError("", "The test is an invalid value.");
+                return NotFound();
             }
+            return View(categoryObjDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
-                _db.SaveChanges(); return RedirectToAction("Index");
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Updated successfully";
+                return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            Category? obj = _db.Categories.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? Id)
+        {
+            Category? obj = _db.Categories.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
