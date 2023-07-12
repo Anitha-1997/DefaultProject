9e0fa838e543ace2dbc404c1cee81dc32069320d
Anitha_97 <anithaks993@gmail.com>
2023-07-13 10:34:19 +1200

    Introducing N Tier Architecture

---
+++ BulkyWebRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWebRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 4, DisplayOrder = 4, Name = "Crime" },
+                new Category { Id = 5, DisplayOrder = 5, Name = "Investigative" },
+                new Category { Id = 6, DisplayOrder = 6, Name = "Romance" }
+                );
+        }
+    }
+}
---
+++ BulkyWebRazor/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "The value is to be less than 100 and greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
